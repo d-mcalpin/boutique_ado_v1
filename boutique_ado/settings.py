824d29849870d26f33527229808a650cf04e6171
d-mcalpin <duncan.mcalpin@gmail.com>
2021-03-17 18:46:45 +0000

    Deployment Part 5 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -171,7 +171,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket Config
+    # Bucket Config - Note Collect Static in Heroku
     AWS_STORAGE_BUCKET_NAME = 'dmc-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
